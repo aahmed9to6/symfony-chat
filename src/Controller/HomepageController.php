0902e9c0c8a0c45f236760490b7fb9bb079756d9
Abdullah Ahmed <abdullah.ahmed@coeus-solutions.de>
2019-03-06 20:29:07 +0500

    Initial commit

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomepageController
+ * @package App\Controller
+ */
+class HomepageController extends AbstractController
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/")
+     */
+    public function homepage(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
